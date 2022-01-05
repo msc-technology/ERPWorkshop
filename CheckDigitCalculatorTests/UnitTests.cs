f6871b5b4d59b034cb209537f1f930cdeaf2e523
Felice Pollano (MSC Technology Italia) <felice.pollano@msc.com>
2022-01-05 10:27:47 +0100

    added more tests

--- CheckDigitCalculatorTests/UnitTests.cs
+++ CheckDigitCalculatorTests/UnitTests.cs
@@ -19,9 +19,35 @@ namespace CheckDigitCalculatorTests
         {
             calc.CalculateChackDigit("MsCU995387");
         }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Invalid code format.")]
         public void throw_if_invalid_format_not_respecting_letters()
         {
             calc.CalculateChackDigit("1SCU995387");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Invalid code format.")]
+        public void throw_if_invalid_format_too_many_letters()
+        {
+            calc.CalculateChackDigit("MSCUU995387");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Invalid code format.")]
+        public void throw_if_invalid_format_too_many_digits()
+        {
+            calc.CalculateChackDigit("MSC1995387");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Invalid code format.")]
+        public void throw_if_invalid_format_too_short()
+        {
+            calc.CalculateChackDigit("MSC199537");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Invalid code format.")]
+        public void throw_if_invalid_format_too_long()
+        {
+            calc.CalculateChackDigit("MSC1995376");
         }
     }
 }
