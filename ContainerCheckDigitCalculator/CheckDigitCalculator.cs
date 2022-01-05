9a63e50106954304ddf5539665e3b2142933a909
Felice Pollano (MSC Technology Italia) <felice.pollano@msc.com>
2022-01-05 11:00:53 +0100

    solved tests for check code

--- ContainerCheckDigitCalculator/CheckDigitCalculator.cs
+++ ContainerCheckDigitCalculator/CheckDigitCalculator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ContainerCheckDigitCalculator
@@ -10,7 +11,15 @@ namespace ContainerCheckDigitCalculator
     {
         public int CalculateCheckDigit(string partialContainerCode)
         {
-            throw new NotImplementedException();
+            EnsureCorrectFormat(partialContainerCode);
+            return -1;
+         
+        }
+
+        private void EnsureCorrectFormat(string partialContainerCode)
+        {
+            if (!Regex.IsMatch(partialContainerCode, @"^[A-Z]{4}\d{6}$"))
+                throw new ArgumentException("Invalid code format.");
         }
     }
 }
