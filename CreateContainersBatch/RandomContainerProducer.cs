a7bd94e9010bbba62eeb152e75f0d0d53d109b34
Felice Pollano (MSC Technology Italia) <felice.pollano@msc.com>
2022-01-05 15:45:08 +0100

    code cleanup

--- CreateContainersBatch/RandomContainerProducer.cs
+++ CreateContainersBatch/RandomContainerProducer.cs
@@ -11,29 +11,7 @@ namespace CreateContainersBatch
     {
         public void GenerateAndSave(int count, IContainerPersisterService persister)
         {
-            string peek = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string[] types =
-            {
-                "20 DRY VAN",
-                "40 DRY VAN",
-                "20 HIGH CUBE",
-                "40 HIGH CUBE",
-            };
-            var rnd = new Random();
-            var calc = new CheckDigitCalculator();
-            persister.BeginPersistence();
-            for (int i = 0; i < count; i++)
-            {
-                string code = string.Concat("MS", peek.Substring(rnd.Next(0, peek.Length), 1),"U");
-                var digits = rnd.Next(99999, 1000000).ToString("000000");
-                var check = calc.CalculateCheckDigit(string.Concat(code, digits));
-                persister.Persist(string.Concat(code, digits, check.ToString()),
-                    types[rnd.Next(0,types.Length)],
-                    DateTime.Today.AddDays(-rnd.Next(0,121))
-                    );
-
-            }
-            persister.EndPersistence();
+            throw new NotImplementedException();
         }
     }
 }
--- CreateContainersBatch/SqlNaivePersister.cs
+++ CreateContainersBatch/SqlNaivePersister.cs
@@ -10,55 +10,22 @@ namespace CreateContainersBatch
 {
     public class SqlNaivePersister : IContainerPersisterService
     {
-        Dictionary<string, int> typeMap = new Dictionary<string, int>();
+        
         public void BeginPersistence()
         {
-            string query = "SELECT * FROM dbo.ContainerType";
-            using (var conn = OpenConnection())
-            {
-                var cmd = conn.CreateCommand();
-                cmd.CommandText = query;
-                cmd.CommandType = CommandType.Text;
-                var reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    typeMap[reader.GetString(reader.GetOrdinal("ContainerTypeCode"))] =
-                        reader.GetInt32(reader.GetOrdinal("ContainerTypeId"));
-                }
-            }
+            throw new NotImplementedException();
         }
 
         public void EndPersistence()
         {
-           
+            throw new NotImplementedException();
         }
 
         public void Persist(string containerCode, string containerType, DateTime hireDate)
         {
-            var insert = "INSERT INTO Container(ContainerCode,ContainerTypeId,HireDate) VALUES (@code,@type,@hire)";
-            if (!typeMap.ContainsKey(containerType))
-            {
-                throw new Exception("invalid type");
-            }
-            using (var conn = OpenConnection())
-            {
-                var cmd = conn.CreateCommand();
-                cmd.CommandText = insert;
-                cmd.CommandType = CommandType.Text;
-                cmd.Parameters.Add(new SqlParameter("code",containerCode));
-                cmd.Parameters.Add(new SqlParameter("type",typeMap[containerType]));
-                cmd.Parameters.Add(new SqlParameter("hire", hireDate));
-                var affected = cmd.ExecuteNonQuery();
-                if (affected != 1)
-                    throw new Exception("data insertion failed without database exception");
-            }
+            throw new NotImplementedException();
         }
 
-        SqlConnection OpenConnection()
-        {
-            var conn = new SqlConnection("data source=.;user=sa;password=wry6WeR56_P?;Initial Catalog=Container");
-            conn.Open();
-            return conn;
-        }
+       
     }
 }
